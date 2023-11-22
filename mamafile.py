f60d5e5152f596f3f9c9c979a5ed6ad8e8a5dfe8
Jorma <RedFox20@users.noreply.github.com>
2023-11-22 17:48:30 +0200

    Create mamafile.py

---
+++ mamafile.py
@@ -0,0 +1,33 @@
+import mama
+from mama.utils.system import console
+from mama.utils.gnu_project import BuildProduct
+
+# Explore Mama docs at https://github.com/RedFox20/Mama
+class netsnmp(mama.BuildTarget):
+
+    local_workspace = 'packages'
+
+    def init(self):
+        self.netsnmp = self.gnu_project('net-snmp', '5.9.4',
+            url='http://kratt.codefox.ee/linux/{{project}}.tar.gz',
+            build_products=[
+                BuildProduct('{{installed}}/lib/libsnmp.a', None),
+            ])
+
+    def settings(self):
+        self.config.prefer_gcc(self.name)
+        if self.mips:
+            self.config.set_mips_toolchain('mipsel')
+
+    def build(self):
+        if self.netsnmp.should_build():
+            opts  = '--disable-shared --enable-static '
+            opts += '--disable-scripts --disable-manuals --disable-mibs '
+            opts += '--disable-ipv6 --disable-embedded-perl --without-pcre '
+            self.netsnmp.build(opts)
+        else:
+            console('lib/libsnmp.a already built', color='green')
+
+    def package(self):
+        self.export_include('net-snmp-built/include', build_dir=True)
+        self.export_lib('net-snmp-built/lib/libsnmp.a', build_dir=True)
